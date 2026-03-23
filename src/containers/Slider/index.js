36340ef9c72d13ee5173cadd13b539da994658d8
Frédéric Sagnimorte <frederic.sagnimorte@gmail.com>
2026-03-23 10:09:18 +0100

    fix: ensure default focus data is set and improve code readability

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,7 +7,7 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = (data?.focus || [])
+  const byDateDesc = (data?.focus || [0,1,2])
   .sort((evtB, evtA) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
@@ -15,17 +15,12 @@ const Slider = () => {
     const timer = setTimeout(() => {
       setIndex(prev => prev < byDateDesc.length - 1 ? prev + 1 : 0);
     }, 5000);
-
     return () => clearTimeout(timer);
   }, [index]);
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <div
-          key={event.title}
-          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"
-            }`}
-        >
+        <div key={event.title} className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
           <img src={event.cover} alt="forum" />
           <div className="SlideCard__descriptionContainer">
             <div className="SlideCard__description">
@@ -40,10 +35,11 @@ const Slider = () => {
         <div className="SlideCard__pagination">
           {byDateDesc?.map((event, radioIdx) => (
             <input
-              key={`${event.id}`}
+              key={event.title}
               type="radio"
               name="radio-button"
               checked={index === radioIdx}
+              onChange={()=>{setIndex(radioIdx)}}
             />
           ))}
         </div>
