9a65e39981cd0b19a7d3747aa3e8d12254dbd98b
Frédéric Sagnimorte <frederic.sagnimorte@gmail.com>
2026-04-03 11:07:36 +0200

    fix: implement tests for getMonth function in Date helper

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -2,14 +2,32 @@
  * 
  */
 
-describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
-    });
-})
+import { getMonth, MONTHS } from "./index"
 
+describe("Date helper",
+    () => {
+        describe("When getMonth is called",
+            () => {
+                it.each(
+                    [
+                        ["janvier", "2022-01-01"],
+                        ["février", "2022-02-01"],
+                        ["mars", "2022-03-01"],
+                        ["avril", "2022-04-01"],
+                        ["mai", "2022-05-01"],
+                        ["juin", "2022-06-01"],
+                        ["juillet", "2022-07-01"],
+                        ["août", "2022-08-01"],
+                        ["septembre", "2022-09-01"],
+                        ["octobre", "2022-10-01"],
+                        ["novembre", "2022-11-01"],
+                        ["décembre", "2022-12-01"],
+                    ]
+                )(`the function return %s for %i as date`, (input, date) => {
+                    expect(getMonth(new Date(date))).toBe(input);
+                })
+            
+            }
+        )
+    }
+)
